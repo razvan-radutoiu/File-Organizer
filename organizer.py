5371655f2f4097b0864bfac474684054b81faf11
razvan-radutoiu <72277109+razvan-radutoiu@users.noreply.github.com>
2024-03-23 17:43:13 +0200

    fix FileExistsError

--- organizer.py
+++ organizer.py
@@ -2,25 +2,28 @@ import os
 import shutil
 
 path = input("Add your path:")
-
 original_locations = {}
 
 for file in os.listdir(path):
     if file.endswith(".py"):
-        continue  # ignore .py files
+        continue # ignore .py files
+
     if os.path.isdir(os.path.join(path, file)):
-        continue  # ignore directories
+        continue # ignore directories
+
     if file.endswith(".lnk"):
-        continue  # ignore shortcuts
+        continue # ignore shortcuts
 
     file_type = file.split(".")[-1].lower()
     if file_type not in os.listdir(path):
         os.mkdir(os.path.join(path, file_type))
-    shutil.move(
-        os.path.join(path, file),
-        os.path.join(path, file_type, file)
-    )
-    print(f"Moved {file} to {os.path.join(path, file_type, file)}")
+
+    destination = os.path.join(path, file_type, file)
+    if os.path.exists(destination):
+        os.remove(destination)
+
+    shutil.move(os.path.join(path, file), destination)
+    print(f"Moved {file} to {destination}")
     original_locations[file] = file_type + "/" + file
 
 choice = input("Enter 1 to keep files organized or 2 to move files back to their original location: ")
@@ -29,8 +32,11 @@ if choice == "2":
     for file, relative_path in original_locations.items():
         original_location = os.path.join(path, relative_path)
         destination = os.path.join(path, file)
+
         if os.path.exists(destination):
             os.remove(destination)
+
         shutil.move(original_location, destination)
         print(f"Moved {file} back to {destination}")
+
     print("Files have been moved back to original directory.")
